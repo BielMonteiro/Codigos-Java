d89639363404381235ea4f61a7fd69685dffab5c
BielMonteiro <38410548+BielMonteiro@users.noreply.github.com>
2018-06-06 09:29:41 -0300

    Lista 2.0

--- EstruturaDeDados/src/Lista/TesteLista.java
+++ EstruturaDeDados/src/Lista/TesteLista.java
@@ -10,36 +10,42 @@ public class TesteLista implements Lista {
 	}
 
 	public Integer insereFinal(Integer valor) {
-		if(!listaCheia()) 
+		if(!listaCheia()) {
 			pos++;
-		else {
-			for (int i = 0; i < pos; i++) {
-				if(i<pos)
-					lista[i]=lista[i+1];
+			lista[pos]=valor;
+		}
+		return valor;
+	}
+	public Integer insereComeco(Integer valor) {
+		if(!listaCheia()) {
+			pos++;
+			for (int i = pos-1; i > 0; i--) {
+				if(i>0)
+					lista[i]=lista[i-1];
+			}
+			lista[0]=valor;
+		}
+		return valor;
+	}
+	public Integer inserePosicao(Integer valor,Integer valor2) {
+		if(valor2>0&&(valor2<=tamLista)) {
+			if(pos<valor2-1) {
+				pos++;
+				lista[pos]=valor;
+			}
+			else{
+				if(lista[valor2-1]==null) {
+					pos++;
+					lista[valor2-1]=valor;
+				}
+				else
+					System.out.println("Posição Ocupada");
 			}
 		}
-		lista[pos]=valor;
+		else
+			System.out.println("O Valor esta fora do interval [1,5]");
 		return valor;
 	}
-
-	public Integer insereComeco(Integer valor) {
-		if(!listaCheia())
-			pos++;
-		for (int i = pos-1; i > 0; i--) {
-			if(i>0)
-				lista[i]=lista[i-1];
-		}
-		lista[0]=valor;
-		return valor;
-	}
-
-	public Integer inserePosicao(Integer valor,Integer valor2) {
-		if(lista[valor2-1]==null)
-			pos++;
-		lista[valor2-1]=valor;
-		return valor;
-	}
-
 	public Integer removeInicial() {
 		if(!listaVazia()) {
 			for (int i = 0; i < pos; i++) {
@@ -50,56 +56,58 @@ public class TesteLista implements Lista {
 			pos--;
 		}
 		return 0;
-		
 	}
-
 	public Integer removeFinal() {
 		if(!listaVazia()) {
-			lista[pos]=null;
-			pos--;
-		}
-		return null;
-	}
-
-	public Integer removePosicao(Integer valor) {
-		if(valor>0&&(valor-1<=pos)&&(valor<=tamLista)) {
-			if(!listaVazia()) {
-				for (int i = valor-1; i < pos; i++) {
-					if(i<pos)
-						lista[i]=lista[i+1];
-				}
+			if(lista[pos]!=null) {
 				lista[pos]=null;
 				pos--;
 			}
+			else
+				System.out.println("Posição Vazia");
 		}
-		else
-			System.out.println("O Valor esta fora do interval [1,5] ou � maior que o tamanho atual da lista");	
 		return null;
 	}
-
+	public Integer removePosicao(Integer valor) {
+		if(valor>0&&(valor<=tamLista)) {
+			if(!listaVazia()) {
+				if(lista[valor-1]!=null) {
+					for (int i = valor-1; i < pos; i++) {
+						if(i<pos)
+							lista[i]=lista[i+1];
+					}
+					lista[pos]=null;
+					pos--;
+				}
+				else
+					System.out.println("Posição Vazia");
+			}
+		}
+		else
+			System.out.println("O Valor esta fora do interval [1,5]");	
+		return null;
+	}
 	public boolean listaVazia() {
-		if(pos==-1)
-			return true;
-		else
-			return false;
-	}
-
-	public boolean listaCheia() {
-		if(pos==tamLista-1)
-			return true;
-		else
-			return false;
-	}
-
-	public void imprimirLista() {
-		if(listaVazia())
+		if(pos==-1) {
 			System.out.println("Lista Vazia");
-		else {
+			return true;
+		}
+		else
+			return false;
+	}
+	public boolean listaCheia() {
+		if(pos==tamLista-1) {
+			System.out.println("lista Cheia");
+			return true;
+		}
+		else
+			return false;
+	}
+	public void imprimirLista() {
+		if(!listaVazia()) {
 			for (int i = 0; i <=pos; i++) {
 				System.out.println(lista[i]);
 			}
 		}
-
 	}
-
 }
