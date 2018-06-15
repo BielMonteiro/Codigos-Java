468c9f8472ef20e15b7e90e40fb51c6797ec5343
BielMonteiro <38410548+BielMonteiro@users.noreply.github.com>
2018-06-14 21:10:10 -0300

    Update Main.java

--- EstruturaDeDados/src/Lista/Main.java
+++ EstruturaDeDados/src/Lista/Main.java
@@ -4,15 +4,17 @@ public class Main {
 
 	public static void main(String[] args) {
 		TesteLista teste = new TesteLista();
-		teste.insereFinal(1);
-		teste.insereFinal(2);
-		teste.insereFinal(3);
-		teste.insereFinal(4);
 		teste.insereFinal(5);
-		teste.insereComeco(5);
-		teste.insereFinal(4);
-		teste.inserePosicao(50,1);;
-		teste.removePosicao(0);
+		teste.insereFinal(10);
+		teste.insereFinal(8);
+		teste.insereFinal(8);
+		teste.insereFinal(10);
+		teste.insereFinal(5);
+		teste.imprimirLista();
+		teste.antePenultimo();
+		teste.alterarLista(1);
+		teste.palindromo();
+		teste.linkUsado(8);
 		teste.imprimirLista();
 	}
 
